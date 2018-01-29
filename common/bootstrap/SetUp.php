0990ddc5d423f3f737a3fbc4ac147f179def597f
Dmitriy <dilife90@gmail.com>
2018-01-29 14:00:46 +0300

    first lesson

---
+++ common/bootstrap/SetUp.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dmitriy
+ * Date: 29.01.18
+ * Time: 12:17
+ */
+
+namespace common\bootstrap;
+
+
+use yii\base\BootstrapInterface;
+
+class SetUp implements BootstrapInterface
+{
+    public function bootstrap($app)
+    {
+        // TODO: Implement bootstrap() method.
+    }
+}
